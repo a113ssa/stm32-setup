2ade30714504f08e4c0f63f11efcd045da9707a4
Kate (ケイト) <95538451+a113ssa@users.noreply.github.com>
2026-02-07 18:45:29 +0400

    Switch connect LCD to use Embassy (#2)

--- lcd/src/main.rs
+++ lcd/src/main.rs
@@ -1,57 +1,51 @@
 #![no_std]
 #![no_main]
 
-use cortex_m_rt::entry;
-use panic_halt as _;
-use stm32l4xx_hal::{ i2c::I2c, pac, prelude::*, i2c::Config, delay::Delay };
+use embassy_executor::Spawner;
+use embassy_stm32::i2c::{Config as I2cConfig, I2c};
+use embassy_stm32::time::Hertz;
+use embassy_stm32::{Config as Stm32Config};
+use embassy_time::{Delay, Timer};
 use hd44780_driver::{ HD44780 };
 
-#[entry]
-fn main() -> ! {
-    let dp = pac::Peripherals::take().unwrap();
-    let cp = cortex_m::Peripherals::take().unwrap();
+use defmt_rtt as _;
+use panic_probe as _;
 
-    let mut flash = dp.FLASH.constrain();
-    let mut rcc = dp.RCC.constrain();
-    let mut pwr = dp.PWR.constrain(&mut rcc.apb1r1);
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) -> ! {
+    // init embassy and peripherals
+    let config = Stm32Config::default();
+    let p = embassy_stm32::init(config);
 
-    let clocks = rcc.cfgr.sysclk(80.MHz()).freeze(&mut flash.acr, &mut pwr);
+    // set i2c configuration
+    let mut i2c_config = I2cConfig::default();
+    i2c_config.frequency = Hertz(100_000); // 100 kHz
 
-    let mut gpiob = dp.GPIOB.split(&mut rcc.ahb2);
-
-    // SCL
-    let scl = gpiob.pb8
-        .into_alternate::<4>(&mut gpiob.moder, &mut gpiob.otyper, &mut gpiob.afrh)
-        .set_open_drain();
-
-    //SDA
-    let sda = gpiob.pb9
-        .into_alternate(&mut gpiob.moder, &mut gpiob.otyper, &mut gpiob.afrh)
-        .set_open_drain();
-
-    // I2C
-    let i2c_config = Config::new(100.kHz(), clocks);
-    let i2c = I2c::i2c1(
-        dp.I2C1,
-        (scl, sda),
+    // lcd driver is synchronous so there is "new_blocking" call
+    let i2c = I2c::new_blocking(
+        p.I2C1,
+        p.PB8, //SCL
+        p.PB9, // SDA
         i2c_config,
-        &mut rcc.apb1r1,
     );
 
-    let mut delay = Delay::new(cp.SYST, clocks);
+    let mut delay = Delay;
 
     let i2c_address = 0x27;
-    let mut lcd = HD44780::new_i2c(i2c, i2c_address, &mut delay).expect("Failed to ini LCD");
+    let mut lcd = HD44780::new_i2c(
+        i2c,
+        i2c_address,
+        &mut delay
+    ).expect("Failed to init LCD");
 
     lcd.clear(&mut delay).unwrap();
 
     lcd.set_cursor_pos(0, &mut delay).unwrap();
-    lcd.write_str("Hello, Rust", &mut delay).unwrap();
-
-    lcd.set_cursor_pos(40, &mut delay).unwrap();
-    lcd.write_str("Embedded Rocks", &mut delay).unwrap();
+    lcd.write_str("Hello, Rust!", &mut delay).unwrap();
+    lcd.set_cursor_pos(42, &mut delay).unwrap();
+    lcd.write_str("  =^_^=    ", &mut delay).unwrap();
 
     loop {
-        cortex_m::asm::nop();
+        Timer::after_millis(1000).await;
     }
 }
