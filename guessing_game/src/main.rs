7d0bbf7906c5ea2f865c6f6d704d811c5bac51f5
a113ssa <alessa.silent.hill.insider@gmail.com>
2026-02-17 19:50:09 +0400

    Add RC module

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -2,42 +2,24 @@
 #![no_main]
 
 use embassy_executor::{Spawner};
+use embassy_stm32::{Config, Peripherals};
 use embassy_stm32::rcc::{Pll, PllMul, PllPreDiv, PllRDiv};
-use embassy_stm32::timer::{CaptureCompareInterruptHandler, Channel};
-use embassy_stm32::timer::input_capture::{CapturePin, InputCapture};
-use embassy_stm32::timer::low_level::CountingMode;
-use embassy_stm32::{Config, Peripherals, bind_interrupts, peripherals, Peri};
-use embassy_stm32::gpio::{Pull};
 use embassy_stm32::rcc::PllSource;
 use embassy_stm32::rcc::Sysclk::{PLL1_R};
 use embassy_stm32::rcc::MSIRange::{RANGE4M};
-use embassy_time::{Delay, Duration, Instant};
-use embassy_stm32::time::{Hertz};
-use embassy_sync::channel::{Channel as SyncChannel, Sender};
+use embassy_sync::channel::{Channel};
 use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
 use heapless::String;
-use infrared::Receiver;
-use infrared::protocol::nec::{Nec16Command,};
-use infrared::receiver::{NoPin};
-use infrared::protocol::{Nec16};
 
 use lcd::{LcdModule, ANSWER_LENGTH};
+use rc::{RcModule, ir_decoder_task};
 
 use defmt_rtt as _;
 use panic_probe as _;
 
 use defmt::info;
 
-static CHANNEL: SyncChannel<CriticalSectionRawMutex, char, 8> = SyncChannel::new();
-
-static RECEIVER_FREQ: u32 = 1_000_000;
-
-static DEBOUNCE_THRESHHOLD: u64 = 300;
-
-
-bind_interrupts!(struct Tim2Interrupt {
-    TIM2 => CaptureCompareInterruptHandler<embassy_stm32::peripherals::TIM2>;
-});
+static CHANNEL: Channel<CriticalSectionRawMutex, char, 8> = Channel::new();
 
 
 #[embassy_executor::main]
@@ -45,94 +27,28 @@ async fn main(spawner: Spawner) -> ! {
     let p: Peripherals = init_peripherals();
 
     let mut lcd_module: LcdModule = LcdModule::new(p.I2C1, p.PB8, p.PB9);
-
-    let rc =  Receiver::<Nec16, NoPin, u32, Nec16Command>::new(RECEIVER_FREQ);
-
-    spawner.spawn(ir_decoder_task(rc, p.TIM2, p.PA0, CHANNEL.sender())).unwrap();
-
-    let mut digits: String<ANSWER_LENGTH> = String::new();
-
     lcd_module.erase();
 
-    loop {
-        let digit = CHANNEL.receive().await;
+    let rc_module: RcModule =  RcModule::new(p.PA0, p.TIM2);
+    spawner.spawn(ir_decoder_task(rc_module, CHANNEL.sender())).unwrap();
 
-        if digit == 'd' {
-            digits.pop();
+    let mut answer: String<ANSWER_LENGTH> = String::new();
+
+    loop {
+        let command: char = CHANNEL.receive().await;
+
+        if command == 'd' {
+            answer.pop();
         } else {
-            digits.push(digit).unwrap();
+            answer.push(command).unwrap();
         }
 
-        if digits.len() > ANSWER_LENGTH - 1 {
+        if answer.len() > ANSWER_LENGTH - 1 {
             lcd_module.erase();
-            digits.clear();
+            answer.clear();
         }
 
-        lcd_module.write(&digits);
-    }
-}
-
-#[embassy_executor::task]
-async fn ir_decoder_task(
-    mut rc: Receiver<Nec16, NoPin, u32, Nec16Command>,
-    tim2: Peri<'static, peripherals::TIM2>,
-    pa0: Peri<'static, peripherals::PA0>,
-    sender: Sender<'static, CriticalSectionRawMutex, char, 8>,
-) -> ! {
-    let mut ic = InputCapture::new(
-        tim2,
-        Some(CapturePin::new(pa0, Pull::Up)),
-        None,
-        None,
-        None,
-        Tim2Interrupt,
-        Hertz(RECEIVER_FREQ),
-        CountingMode::EdgeAlignedUp,
-    );
-
-    let mut last_capture: u32 = 0;
-    let mut edge = true;
-
-    let mut last_processed_time = Instant::now();
-    let debounce_threshhold = Duration::from_millis(DEBOUNCE_THRESHHOLD);
-
-    loop {
-        let now = ic.wait_for_any_edge(Channel::Ch1).await;
-        let dt = now.wrapping_sub(last_capture);
-        last_capture = now;
-
-        match rc.event(dt, edge) {
-            Ok(Some(cmd)) => {
-                let current_processed_time = Instant::now();
-                if current_processed_time.duration_since(last_processed_time) >= debounce_threshhold {
-                    last_processed_time = Instant::now();
-                    let digit: Option<char> = match cmd.cmd {
-                        22 => Some('0'),
-                        12 => Some('1'),
-                        24 => Some('2'),
-                        94 => Some('3'),
-                        8 => Some('4'),
-                        28 => Some('5'),
-                        90 => Some('6'),
-                        66 => Some('7'),
-                        82 => Some('8'),
-                        74 => Some('9'),
-                        68 => Some('d'),
-                        64 => Some('e'),
-                        _ => None,
-                    };
-
-                    if let Some(d) = digit {
-                        sender.send(d).await;
-                    }
-                } else {}
-            }
-
-            Ok(None) => {}
-            Err(_e) => {}
-        }
-
-        edge = !edge;
+        lcd_module.write(&answer);
     }
 }
 
@@ -154,3 +70,4 @@ fn init_peripherals() -> Peripherals {
 }
 
 mod lcd;
+mod rc;
---
+++ guessing_game/src/rc.rs
@@ -0,0 +1,102 @@
+use embassy_stm32::{
+    Peri,
+    bind_interrupts,
+    gpio::Pull,
+    peripherals,
+    time::Hertz,
+    timer::{
+        CaptureCompareInterruptHandler,
+        Channel,
+        input_capture::{CapturePin, InputCapture},
+        low_level::CountingMode
+    }};
+use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, channel::Sender};
+use embassy_time::{Duration, Instant};
+use infrared::{Receiver, protocol::{Nec16, nec::Nec16Command}, receiver::NoPin};
+
+static RECEIVER_FREQ: u32 = 1_000_000;
+static DEBOUNCE_THRESHHOLD: u64 = 300;
+static DEBOUNCE_DURATION: Duration = Duration::from_millis(DEBOUNCE_THRESHHOLD);
+
+bind_interrupts!(struct Tim2Interrupt {
+    TIM2 => CaptureCompareInterruptHandler<embassy_stm32::peripherals::TIM2>;
+});
+
+pub struct RcModule {
+    receiver: Receiver::<Nec16, NoPin, u32, Nec16Command>,
+    input_capture: InputCapture<'static, peripherals::TIM2>,
+}
+
+impl RcModule {
+    pub fn new(
+        input_capture_pin: Peri<'static, peripherals::PA0>,
+        tim2: Peri<'static, peripherals::TIM2>,
+    ) -> Self {
+        Self {
+            receiver: Receiver::<Nec16, NoPin, u32, Nec16Command>::new(RECEIVER_FREQ),
+            input_capture: InputCapture::new(
+                tim2,
+                Some(CapturePin::new(input_capture_pin, Pull::Up)),
+                None,
+                None,
+                None,
+                Tim2Interrupt,
+                Hertz(RECEIVER_FREQ),
+                CountingMode::EdgeAlignedUp,
+            )
+        }
+    }
+
+}
+
+#[embassy_executor::task]
+pub async fn ir_decoder_task(mut rc_module: RcModule, sender: Sender<'static, CriticalSectionRawMutex, char, 8>) -> ! {
+    let mut last_capture: u32 = 0;
+    let mut last_edge: bool = true;
+    let mut last_processed_time: Instant = Instant::now();
+
+    loop {
+        let current_capture: u32 = rc_module.input_capture.wait_for_any_edge(Channel::Ch1).await;
+        let delta_time: u32 = current_capture.wrapping_sub(last_capture);
+        last_capture = current_capture;
+
+        match &mut rc_module.receiver.event(delta_time, last_edge) {
+            Ok(Some(cmd)) => {
+                let current_processed_time = Instant::now();
+
+                if current_processed_time.duration_since(last_processed_time) >= DEBOUNCE_DURATION {
+                    last_processed_time = Instant::now();
+
+                    let command: Option<char> = map_command(cmd.cmd);
+
+                    match command {
+                        Some(c) => sender.send(c).await,
+                        _ => {}
+                    }
+                }
+            }
+            Ok(None) => {}
+            Err(_e) => {}
+        }
+
+        last_edge = !last_edge;
+    }
+}
+
+fn  map_command(command: u8) -> Option<char> {
+    return match command {
+        22 => Some('0'),
+        12 => Some('1'),
+        24 => Some('2'),
+        94 => Some('3'),
+        8 => Some('4'),
+        28 => Some('5'),
+        90 => Some('6'),
+        66 => Some('7'),
+        82 => Some('8'),
+        74 => Some('9'),
+        68 => Some('d'),
+        64 => Some('e'),
+        _ => None,
+    };
+}
